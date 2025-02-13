eb8ae607b32ba9451a5b866b9a1094a96a599b7b
wilbertgongora <211k0039@itscarrillopuerto.edu.mx>
2025-02-13 15:30:54 -0500

    Update index.js

--- Ejercicios/Ejercicios/1.2 Run file/index.js
+++ Ejercicios/Ejercicios/1.2 Run file/index.js
@@ -162,29 +162,6 @@
 // moto.encender();
 // moto.apagar();
 
-// Ejercicio 9
-
-// // main.js
-
-// // Importamos el módulo mathOperations
-// const mathOps = require('./mathOperations');
-
-// // Usamos las funciones del módulo
-// const num1 = 10;
-// const num2 = 5;
-
-// console.log(`Suma: ${mathOps.suma(num1, num2)}`);
-// console.log(`Resta: ${mathOps.resta(num1, num2)}`);
-// console.log(`Multiplicación: ${mathOps.multiplicacion(num1, num2)}`);
-// console.log(`División: ${mathOps.division(num1, num2)}`);
-
-// // Manejo de errores en la división
-// try {
-//     console.log(`División por cero: ${mathOps.division(num1, 0)}`);
-// } catch (error) {
-//     console.log(error.message);
-// }
-
 
 //Ejercicio 10 
 
